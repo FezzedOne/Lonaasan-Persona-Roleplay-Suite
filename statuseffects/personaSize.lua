f0698f291ce1414a2057437a3d6e904b5b658b46
Lonaasan <shadowlona@shadowlona.dev>
2025-10-15 19:23:25 +0200

    feat: better resize

---
+++ statuseffects/personaSize.lua
@@ -0,0 +1,19 @@
+
+local oldSize = 1
+
+function init()
+
+end
+
+function update(dt)
+
+  local playerSize = status.statusProperty("personaSize", 1) or 1
+  if playerSize ~= oldSize then
+    oldSize = playerSize
+    effect.setParentDirectives("?scalenearest=" .. tostring(playerSize))
+  end
+end
+
+function uninit()
+  
+end
